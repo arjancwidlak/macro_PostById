8f3e9e2716b2b1bcc3b02b488ff3956718261436
Arjan Widlak <arjan@unitedknowledge.nl>
2011-05-19 17:16:36 +0200

    Added some documentation

--- lib/WebGUI/Macro/PostById.pm
+++ lib/WebGUI/Macro/PostById.pm
@@ -1,18 +1,13 @@
 package WebGUI::Macro::PostById;
 
-#-------------------------------------------------------------------
-# WebGUI is Copyright 2001-2005 Plain Black Corporation.
-#-------------------------------------------------------------------
-# Please read the legal notices (docs/legal.txt) and the license
-# (docs/license.txt) that came with this distribution before using
-# this software.
-#-------------------------------------------------------------------
-# http://www.plainblack.com                     info@plainblack.com
-#-------------------------------------------------------------------
+#-------------------------------------------------------------------------------
+# Author: Arjan Widlak, United Knowledge 2011
+#-------------------------------------------------------------------------------
+# http://www.unitedknowledge.nl                    serverinfo@unitedknowledge.nl
+#-------------------------------------------------------------------------------
 
 use strict;
-#use WebGUI::Asset;
-#use WebGUI::Asset::Post;
+use WebGUI::Asset::Post;
 
 =head1 NAME
 
@@ -39,33 +34,33 @@ to what's in the post_loop of the collaboration template.
 #-------------------------------------------------------------------
 
 sub process {
-        my $session = shift;
+    my $session = shift;
 
-        my $assetId = shift;
+    my $assetId = shift;
 	my $templateId = shift;
 
 	my %var;
 	
 	# Create an instance of the Post, by its assetId.
-        my $post = WebGUI::Asset->newByDynamicClass($session, $assetId);
+    my $post = WebGUI::Asset->newByDynamicClass($session, $assetId);
 
 	# Start collecting the data that will be used in the template:
-        my @rating_loop;
-        for (my $i=0;$i<=$post->get("rating");$i++) {
-        	push(@rating_loop,{'rating_loop.count'=>$i});
-        }
-        my %lastReply;
-        my $hasRead = 0;
-        if ($post->get("className") =~ /Thread/) {
-                $hasRead = $post->isMarkedRead;
-        }
-        my $url;
-        if ($post->get("status") eq "pending") {
-        	$url = $post->getUrl("revision=".$post->get("revisionDate"))."#".$post->getId;
-        } else {
-        	$url = $post->getUrl."#".$post->getId;
-        }
-        $var{'id'} = $post->getId;
+    my @rating_loop;
+    for (my $i=0;$i<=$post->get("rating");$i++) {
+    	push(@rating_loop,{'rating_loop.count'=>$i});
+    }
+    my %lastReply;
+    my $hasRead = 0;
+    if ($post->get("className") =~ /Thread/) {
+            $hasRead = $post->isMarkedRead;
+    }
+    my $url;
+    if ($post->get("status") eq "pending") {
+        $url = $post->getUrl("revision=".$post->get("revisionDate"))."#".$post->getId;
+    } else {
+        $url = $post->getUrl."#".$post->getId;
+    }
+    $var{'id'} = $post->getId;
 	$var{'title'} = $post->getTitle;
 	$var{'username'} = $post->get("username");
 	$var{'synopsis'} = $post->get("synopsis");
@@ -77,24 +72,41 @@ sub process {
 	$var{"url"}= $url;
 	$var{"rating_loop"} = \@rating_loop;
 	$var{"content"} = $post->formatContent;
-        $var{"status"} = $post->getStatus;
-        $var{"thumbnail"} = $post->getThumbnailUrl;
-        $var{"image.url"} = $post->getImageUrl;
+    $var{"status"} = $post->getStatus;
+    $var{"thumbnail"} = $post->getThumbnailUrl;
+
+    my $storage = $post->getStorageLocation;
+    ( $var{"image.url"}, $var{"image.size"} ) = getImageUrl( $storage );
+
 	#$var{"dateSubmitted"} = $post->get("dateSubmitted");
 	$var{"dateSubmitted"} = $post->get("creationDate");
-        $var{"dateSubmitted.human"} = $session->datetime->epochToHuman( $post->get("creationDate") );
-        $var{"userProfile.url"} = $post->getPosterProfileUrl;
-        $var{"user.isVisitor"} = $post->get("ownerUserId") eq "1";
-        $var{"edit.url"} = $post->getEditUrl;
+    $var{"dateSubmitted.human"} = $session->datetime->epochToHuman( $post->get("creationDate") );
+    $var{"userProfile.url"} = $post->getPosterProfileUrl;
+    $var{"user.isVisitor"} = $post->get("ownerUserId") eq "1";
+    $var{"edit.url"} = $post->getEditUrl;
 	my $controls = $session->icon->delete('func=delete',$post->get("url"),"Delete").$session->icon->edit('func=edit',$post->get("url"));
-        $var{'controls'} = $controls;
-        $var{"user.hasRead"} = $hasRead;
-        $var{"user.isPoster"} = $post->isPoster;
+    $var{'controls'} = $controls;
+    $var{"user.hasRead"} = $hasRead;
+    $var{"user.isPoster"} = $post->isPoster;
 
-        $post->getTemplateMetadataVars(\%var);
+    $post->getTemplateMetadataVars(\%var);
 	if ($post->get("className") =~ /Thread/) {
 		$var{'rating'} = $post->get('threadRating');
 	}
 	return WebGUI::Asset::Template->new($session,$templateId)->process(\%var);
+}
+
+sub getImageUrl {
+	my $storage = shift;
+	return undef if ($storage->getId eq "");
+	my ( $url, $filesize );
+	foreach my $filename (@{$storage->getFiles}) {
+		if ( $storage->isImage( $filename ) ) {
+			$url        = $storage->getUrl( $filename );
+            $filesize   = $storage->getFileSize( $filename );
+			last;
+		}
+	}
+	return wantarray ? ( $url, $filesize ) : $url;
 }
 1;
